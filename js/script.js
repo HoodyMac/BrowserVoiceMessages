9fd66e4acf11a385bedb8a2d0aedb690f5646362
Maksym Osmuk <maxim.osmuk@gmail.com>
2017-02-08 21:27:29 +0100

    Add app sending and reciving message script

--- js/script.js
+++ js/script.js
@@ -8,10 +8,29 @@ angular.module('voiceMessagesApp', ['ngRoute'])
               redirectTo: '/'
             });
   })
-  .controller('VoiceMessagesController', function($scope, $routeParams) {
-    $scope.message = $routeParams.message;
-    console.log($routeParams);
+  .controller('VoiceMessagesController', function($scope, $routeParams, $location) {
+    var baseUrl = $location.protocol() + '://' + $location.host()
+                  + ':' + $location.port() + '/#!/';
+    console.log(baseUrl);
+    $scope.message = ''
     $scope.link = "https://github.com";
+    $scope.$on('$routeChangeSuccess', function() {
+      if ($routeParams.message) {
+        var message = $routeParams.message;
+        notify(message);
+      }
+    });
+
+    $scope.getLink = function () {
+      $scope.link = baseUrl + $scope.message;
+    };
+
+    function notify(message) {
+      var voices = speechSynthesis.getVoices();
+      var utterance = new SpeechSynthesisUtterance(message);
+      utterance.voice = voices[0];
+      speechSynthesis.speak(utterance);
+    }
 
     // CryptoJS.AES.encrypt("Message", "Secret Passphrase")
     // CryptoJS.AES.decrypt(encrypted, "Secret Passphrase")
