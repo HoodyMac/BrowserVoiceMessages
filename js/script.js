6a54a4c2e994ab8693a1825a64f2880cda45696b
Maksym Osmuk <maxim.osmuk@gmail.com>
2017-02-08 21:03:30 +0100

    Add basic structure and styling for an app

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-angular.module('voiceMessagesApp', [])
-  .controller('VoiceMessagesController', function($scope) {
-    $scope.message = "Hi there";
+angular.module('voiceMessagesApp', ['ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+            .when('/:message', {
+              templateUrl: 'index.html',
+              controller: 'VoiceMessagesController'
+            }).otherwise( {
+              redirectTo: '/'
+            });
+  })
+  .controller('VoiceMessagesController', function($scope, $routeParams) {
+    $scope.message = $routeParams.message;
+    console.log($routeParams);
+    $scope.link = "https://github.com";
+
+    // CryptoJS.AES.encrypt("Message", "Secret Passphrase")
+    // CryptoJS.AES.decrypt(encrypted, "Secret Passphrase")
   });
