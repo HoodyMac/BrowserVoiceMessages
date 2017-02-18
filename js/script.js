8b2a9b8387ae9b648645d4e862ba931f813af967
Maksym Osmuk <maxim.osmuk@gmail.com>
2017-02-18 21:31:59 +0100

    Fix encryption for all characters

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ angular.module('voiceMessagesApp', ['ngRoute', 'ngClipboard'])
 
     $scope.getLink = function () {
       var encryptedMessage = caesarShift($scope.message, key);
-      encryptedMessage = encryptedMessage.replace(' ', '%20');
+      encryptedMessage = encryptedMessage.replace(/ /g, '%20');
       $scope.link = baseUrl + encryptedMessage;
     };
 
@@ -53,20 +53,16 @@ angular.module('voiceMessagesApp', ['ngRoute', 'ngClipboard'])
     }
 
     function caesarShift(str, amount) {
-    	if (amount < 0)
-    		return caesarShift(str, amount + 26);
     	var output = '';
-    	for (var i = 0; i < str.length; i ++) {
-    		var c = str[i];
-    		if (c.match(/[a-z]/i)) {
-    			var code = str.charCodeAt(i);
-    			if ((code >= 65) && (code <= 90))
-    				c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
-    			else if ((code >= 97) && (code <= 122))
-    				c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
-    		}
-    		output += c;
-    	}
+      for (var i = 0; i < str.length; i++) {
+        char = str[i].charCodeAt();
+        if(char === 32) {
+          output += ' ';
+          continue;
+        }
+        charcode = char + amount;
+        output += String.fromCharCode(charcode);
+      }
     	return output;
-    };
+    }
   });
